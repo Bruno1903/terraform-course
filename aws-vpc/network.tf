65fe4f6c7acc0d374ea6027b8584da4573a25d36
Bruno1903 <brunoalmeida1960@gmail.com>
2024-07-18 20:18:57 -0300

    criando route table

--- aws-vpc/network.tf
+++ aws-vpc/network.tf
@@ -22,3 +22,21 @@ resource "aws_internet_gateway" "internet_gateway" {
     Name = "internet-gateway-terraform"
   }
 }
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "route-table-terraform"
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
