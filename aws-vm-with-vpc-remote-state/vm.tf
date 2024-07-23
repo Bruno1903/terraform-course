a7e220f3b58f84d93f4e2d9b546253b77d841f57
Bruno1903 <brunoalmeida1960@gmail.com>
2024-07-22 21:53:36 -0300

    adicionando ami

--- aws-vm-with-vpc-remote-state/vm.tf
+++ aws-vm-with-vpc-remote-state/vm.tf
@@ -4,7 +4,7 @@ resource "aws_key_pair" "key" {
 }
 
 resource "aws_instance" "vm" {
-  ami                     = ""
+  ami                     = "ami-01a38093d387a7497"
   instance_type           = "t2.micro"
   key_name = aws_key_pair.key.key_name
   subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
